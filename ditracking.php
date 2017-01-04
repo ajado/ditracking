8137af3b09377a69bc2aad0ce951678692758660
Matteo <matteo@digitalideas.io>
2017-01-04 18:51:26 +0100

    live changes

--- ditracking.php
+++ ditracking.php
@@ -7,6 +7,8 @@
  * Author URI: http://www.digitalideas.io
  */
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+defined( 'DITRACKING_PUSHOVER_APP_KEY' ) or die( 'Please define DITRACKING_PUSHOVER_APP_KEY' );
+defined( 'DITRACKING_PUSHOVER_USER_KEY' ) or die( 'Please define DITRACKING_PUSHOVER_USER_KEY' );
 
 function divoluum_add_query_vars_filter( $vars ){
   $vars[] = "cid";
@@ -18,38 +20,61 @@ add_action('wp', 'divoluum_track', 1, 0);
 
 function divoluum_track($result, $orderId) {
 	if ( !is_admin() ) {
-		$cid = get_query_var('cid', NULL);
-		if(!empty($cid)) {
-			setcookie("civoluum_cid", $cid, time() + (86400 * 30), "/"); 
+		$cidToStore = get_query_var('cid', NULL);
+		if(!empty($cidToStore)) {
+			setcookie("civoluum_cid", $cidToStore, time() + (86400 * 30), "/"); 
 		}
 		
+		$doVoluumPostback = array_shift(get_post_custom_values('do_voluum_postback'));
 		
-		/*
-			
-			$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL,"http://www.mysite.com/tester.phtml");
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,
-            "postvar1=value1&postvar2=value2&postvar3=value3");
-
-// in real life you should use something like:
-// curl_setopt($ch, CURLOPT_POSTFIELDS, 
-//          http_build_query(array('postvar1' => 'value1')));
-
-// receive server response ...
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$server_output = curl_exec ($ch);
-
-curl_close ($ch);
-
-// further processing ....
-if ($server_output == "OK") { ... } else { ... }
-
-*/
-		
-		//$voluumProductIdToDoPostbackFor = intval(array_shift(get_post_custom_values('voluumPostbackProductId')));
+		if($doVoluumPostback == 'YES') {
+			if(isset($_COOKIE['civoluum_cid']) && (!empty($_COOKIE['civoluum_cid']))) {
+				$cidToDoPostbackWith = $_COOKIE['civoluum_cid'];
+				$query = http_build_query(array('cid' => $cidToDoPostbackWith));
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, "http://c2shp.voluumtrk2.com/postback");
+				curl_setopt($ch, CURLOPT_POST, 1);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(
+						array(
+							'cid' => $cidToDoPostbackWith
+						)
+					));
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				$returnInfo = curl_exec($ch);
+				
+				$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+				curl_close ($ch);
+				
+				if($httpCode == 400) {
+					curl_setopt_array($ch = curl_init(), array(
+                      CURLOPT_URL => "https://api.pushover.net/1/messages.json",
+                      CURLOPT_POSTFIELDS => array(
+                        "token" => DITRACKING_PUSHOVER_APP_KEY,
+                        "user" => DITRACKING_PUSHOVER_USER_KEY,
+                        "message" => "Got a 400 error when doing postback with CID $cidToDoPostbackWith returned $$returnInfo on //$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",
+                      ),
+                      CURLOPT_SAFE_UPLOAD => true,
+                      CURLOPT_RETURNTRANSFER => true,
+                    ));
+                    curl_exec($ch);
+                    curl_close($ch);
+				}
+			}
+			else {
+    			curl_setopt_array($ch = curl_init(), array(
+                  CURLOPT_URL => "https://api.pushover.net/1/messages.json",
+                  CURLOPT_POSTFIELDS => array(
+                    "token" => DITRACKING_PUSHOVER_APP_KEY,
+                    "user" => DITRACKING_PUSHOVER_USER_KEY,
+                    "message" => "(Test) postback not set on //$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",
+                  ),
+                  CURLOPT_SAFE_UPLOAD => true,
+                  CURLOPT_RETURNTRANSFER => true,
+                ));
+                $return = curl_exec($ch);
+                curl_close($ch);
+			}
+		}
 	}
 	return true;
 }
